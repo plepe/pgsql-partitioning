5ae371d7bdd0aac26a595c7a7ede0f902455e752
Stephan Plepelits <skunk@xover.mud.at>
2012-09-29 19:34:12 +0200

    Partition Integer: (re-)create all table functions

--- modules/partition_integer/functions.sql
+++ modules/partition_integer/functions.sql
@@ -21,33 +21,6 @@ BEGIN
   -- add table to the list of partition_tables
   insert into partition_tables values (table_name, Array[]::text[], Array[]::text[], options);
 
-  -- create trigger for insert statement
-  fun='create or replace function partition_integer_insert_trigger_'||table_name||'() returns trigger as $f$ DECLARE part_id int8; BEGIN part_id=(NEW.'||(options->'id_column')||'/'||(options->'id_div')||')&'||(options->'id_mask')||';';
-  fun=fun||build_if_tree(0, cast(options->'id_mask' as int), 'part_id', 'insert into '||table_name||'_% values (NEW.*)');
-  fun=fun||' return null; END; $f$ language plpgsql;';
-  execute fun;
-
-  -- set trigger on insert statement
-  execute 'create trigger partition_integer_insert_trigger_'||table_name||' before insert on '||table_name||' for each row execute procedure partition_integer_insert_trigger_'||table_name||'();';
-
-  for i in 0..options->'id_mask' loop
-    execute 'create table '||table_name||'_'||i||' () '||
-      ' inherits ('||table_name||');';
-
-    perform partition_table_indexes(table_name, cast(i as text));
-  end loop;
-
-  -- create query function
-  fun='create or replace function '||table_name||'(in id int8, in _where text default null, in options hstore default ''''::hstore) returns setof '||table_name||' as $f$ ';
-  fun=fun||'declare r '||table_name||'%rowtype; sql text; part_id int; begin ';
-  fun=fun||'part_id=(id/'||(options->'id_div')||')&'||(options->'id_mask')||';';
-  fun=fun||'sql:=''select * from '||table_name||'_''||part_id||'' where "'||(options->'id_column')||'"=''||id; ';
-  fun=fun||'if _where is not null then sql=sql||'' and (''||_where||'')''; end if;';
-  -- fun=fun||'raise notice ''%'', sql; ';
-  fun=fun||'return query execute sql;';
-  fun=fun||'return; end; $f$ language plpgsql;';
-  execute fun;
-
   -- save list of current indexes
   for r in execute 'select * from pg_indexes where tablename='''||table_name||'''' loop
     index_def=array_append(index_def, r.indexdef);
@@ -56,10 +29,21 @@ BEGIN
     set indexes=index_def
     where partition_tables.table_name=table_name;
 
+  -- create all sub-tables, create indexes
+  for i in 0..options->'id_mask' loop
+    execute 'create table '||table_name||'_'||i||' () '||
+      ' inherits ('||table_name||');';
+
+    perform partition_table_indexes(table_name, cast(i as text));
+  end loop;
+
+  -- create functions and triggers
+  perform partition_integer_update_functions(table_name);
+
   -- move current data from table to other-subtable
   execute 'insert into '||table_name||' (select * from only '||table_name||');';
   execute 'delete from only '||table_name||';';
-  
+
   return true;
 END;
 $$ LANGUAGE plpgsql;
@@ -99,3 +83,36 @@ BEGIN
   return true;
 END;
 $$ language plpgsql;
+
+CREATE OR REPLACE FUNCTION partition_integer_update_functions(in table_name text) returns boolean as $$
+#variable_conflict use_variable
+DECLARE
+  fun text;
+  options hstore;
+BEGIN
+  select partition_tables.options into options from partition_tables where partition_tables.table_name=table_name;
+
+  -- create insert function which will be called by the trigger
+  fun='create or replace function partition_integer_insert_trigger_'||table_name||'() returns trigger as $f$ DECLARE part_id int8; BEGIN part_id=(NEW.'||(options->'id_column')||'/'||(options->'id_div')||')&'||(options->'id_mask')||';';
+  fun=fun||build_if_tree(0, cast(options->'id_mask' as int), 'part_id', 'insert into '||table_name||'_% values (NEW.*)');
+  fun=fun||' return null; END; $f$ language plpgsql;';
+  execute fun;
+
+  -- create trigger on insert statement
+  execute 'drop trigger if exists partition_integer_insert_trigger_'||table_name||' on '||table_name||';';
+  execute 'create trigger partition_integer_insert_trigger_'||table_name||' before insert on '||table_name||' for each row execute procedure partition_integer_insert_trigger_'||table_name||'();';
+
+  -- create query function
+  fun='create or replace function '||table_name||'(in id int8, in _where text default null, in options hstore default ''''::hstore) returns setof '||table_name||' as $f$ ';
+  fun=fun||'declare r '||table_name||'%rowtype; sql text; part_id int; begin ';
+  fun=fun||'part_id=(id/'||(options->'id_div')||')&'||(options->'id_mask')||';';
+  fun=fun||'sql:=''select * from '||table_name||'_''||part_id||'' where "'||(options->'id_column')||'"=''||id; ';
+  fun=fun||'if _where is not null then sql=sql||'' and (''||_where||'')''; end if;';
+  -- fun=fun||'raise notice ''%'', sql; ';
+  fun=fun||'return query execute sql;';
+  fun=fun||'return; end; $f$ language plpgsql;';
+  execute fun;
+
+  return true;
+END;
+$$ LANGUAGE plpgsql;
