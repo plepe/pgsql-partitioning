cddb10b02a81a10567083814958aa31382113814
Stephan Plepelits <skunk@xover.mud.at>
2012-09-29 15:06:01 +0200

    Parition: Adapt to 'modulekit'-structure

---
+++ modules/partition/modulekit.php
@@ -0,0 +1,11 @@
+<?
+$name="Separates a table into several parts by a where clause";
+
+$include=array(
+  'pgsql-functions'=>array(
+    "functions.sql",
+  ),
+  'pgsql-init'=>array(
+    "init.sql",
+  ),
+);
