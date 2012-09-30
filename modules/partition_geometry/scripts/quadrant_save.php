3cabf161a581c7b7847ad0cdd25c29434ae526b6
Stephan Plepelits <skunk@xover.mud.at>
2012-09-30 07:34:21 +0200

    Partition Geometry::Quadrant Save: Change structure, directly write PG files

--- modules/partition_geometry/scripts/quadrant_save.php
+++ modules/partition_geometry/scripts/quadrant_save.php
@@ -9,8 +9,10 @@ $y_step_size=($y_axis[1]-$y_axis[0])/$y_steps;
 
 $res=sql_query("select * from quadrant_part order by y_min, x_min");
 
-$f=fopen(sprintf($save_name, pg_num_rows($res)), "w");
-fwrite($f, "boundary\tobject_count\n");
+$f_csv=fopen(sprintf($save_name, pg_num_rows($res)).".csv", "w");
+$f_srid=fopen(sprintf($save_name, pg_num_rows($res)).".".$SRID, "w");
+$f_4326=fopen(sprintf($save_name, pg_num_rows($res)).".4326", "w");
+fwrite($f_csv, "x_min\ty_min\tx_max\ty_max\tobject_count\tassess\n");
 
 while($elem=pg_fetch_assoc($res)) {
   $x_min=$x_axis[0]+$elem['x_min']*$x_step_size;
@@ -18,7 +20,15 @@ while($elem=pg_fetch_assoc($res)) {
   $y_min=$y_axis[0]+$elem['y_min']*$y_step_size;
   $y_max=$y_axis[0]+($elem['y_max']+1)*$y_step_size;
 
-  fwrite($f, "ST_MakeEnvelope($x_min, $y_min, $x_max, $y_max, $SRID)\t{$elem['count']}\n");
+  fwrite($f_csv, "{$elem['x_min']}\t{$elem['y_min']}\t{$elem['x_max']}\t{$elem['y_max']}\t{$elem['count']}\t{$elem['assess']}\n");
+
+  $res_bbox=sql_query("select ST_MakeEnvelope($x_min, $y_min, $x_max, $y_max, $SRID) as geom_srid, ST_Transform(ST_MakeEnvelope($x_min, $y_min, $x_max, $y_max, $SRID), 4326) as geom_4326");
+  $elem_bbox=pg_fetch_assoc($res_bbox);
+
+  fwrite($f_srid, "{$elem_bbox['geom_srid']}\n");
+  fwrite($f_4326, "{$elem_bbox['geom_4326']}\n");
 }
 
-fclose($f);
+fclose($f_csv);
+fclose($f_srid);
+fclose($f_4326);
